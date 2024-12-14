6b5f845436acda1721764f48e7d6665439b1772e
Neevil <91553852+ZarXD@users.noreply.github.com>
2024-12-14 11:24:20 +0700

    Pertemuan 8

---
+++ latihan7/array.php
@@ -0,0 +1,56 @@
+<?php
+// Create array
+
+# Expression
+# $arr = array("budiono", "jonathan", "hansen");
+# $arr = ["budiono", "jonathan", "hansen"];
+
+# Indexed array (Index Numerik)
+$arr = array("budiono", "jonathan", "hansen");
+echo $arr[2]."<br>";
+
+$arr = [];
+$arr = ["fahad", "rafi", "bayu"];
+
+# Associative array (Index Name)
+$mobil = array(
+    "brand" => "Toyota",
+    "model" => "Agya",
+    "year" => "2022",
+);
+echo $mobil["brand"]."<br>";
+
+$myArr = [];
+$myArr[0] = "apel";
+$myArr[1] = "jambu";
+$myArr[2] = "pisang";
+
+var_dump($myArr);
+
+echo "<br>";
+# Function on array
+
+function myFunc() {
+    echo "Saya MHS ASE";
+}
+echo "<br>";
+
+
+$myArr = array(
+    "motor" => "PCX",
+    "age" => 18,
+    "message" => "myFunc",
+);
+
+echo $myArr["message"]();
+echo "<br>";
+
+
+
+
+// Multidimension array
+$buah = ["pisang", "mangga", "apel"];
+array_push($buah, ["semangka", "nanas", "jeruk"], "nangka", "durian");
+
+var_dump($buah);
+?>
---
+++ latihan7/shopping.php
@@ -0,0 +1,68 @@
+<?php 
+
+$database_list = ["Apel", "Pisang", "Jeruk"];
+
+function display_db() {
+    global $database_list;
+    if (is_array($database_list) && !empty($database_list)) {
+        echo "<ul>";
+        foreach ($database_list as $items) {
+            echo "<li>". htmlspecialchars($items) ."</li>";
+        }
+        echo "</ul>";
+    } else {
+        echo "Daftar Kosong";
+    }
+} 
+
+function addItem($item) {
+    global $database_list;
+    array_push($database_list, $item);
+}
+
+function deleteItem($item) {
+    global $database_list;
+    $index = array_search($item, $database_list, true);
+    if ($index !== false) {
+        unset($database_list[$index]);
+        $database_list = array_values( $database_list);
+    }
+}
+
+if (!empty($_POST)) {
+    if (!empty($_POST["new_item"])) {
+        addItem($_POST["new_item"]);
+    }
+    if (!empty($_POST["delete_item"])) {
+        deleteItem($_POST["delete_item"]);
+    }
+} else {
+    
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shopping</title>
+</head>
+<body>
+    <h1>Daftar Belanja</h1>
+    <?php display_db() ?>
+
+    <h2>Tambah Item</h2>
+    <form method="post">
+        <input type="text" name="new_item" placeholder="Tambah item baru" required>
+        <input type="submit" value="Tambah">
+    </form>
+
+    <h2>Hapus Item</h2>
+    <form method="post">
+        <input type="text" name="delete_item" placeholder="Tambah item baru" required>
+        <input type="submit" value="Hapus">
+    </form>
+</body>
+</html>
